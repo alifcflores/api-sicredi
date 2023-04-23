4329f89331ba66b8ac8d9e5a0a762e4701dd615b
Alif Corrêa Flores <alifcorrea.qa@gmail.com>
2023-04-23 20:58:15 -0300

    include more scenarios and refine existents

--- src/test/java/com/api/simulationTest.java
+++ src/test/java/com/api/simulationTest.java
@@ -3,29 +3,39 @@ package com.api;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import org.json.simple.*;
+import org.junit.Before;
 import org.junit.Test;
+
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import com.github.javafaker.*;
 
 public class simulationTest {
 
-    public static int randomNumber() {
-        return (int) Math.floor(Math.random() * (9 - 0 + 1) + 0);
+    @Before
+    public void initialConfigs() {
+        RestAssured.baseURI = "http://localhost:8080";
+        RestAssured.basePath = "/api/v1";
     }
+
+    Faker faker = new Faker(new Locale("pt-BR"));
+    String name = faker.name().name();
+    int monetaryValue = faker.number().numberBetween(1000, 20000);
+    int installments = faker.number().numberBetween(1, 60);
 
     String[] cpfsWithRestriction = { "97093236014", "60094146012", "84809766080", "62648716050",
             "26276298085", "01317496094", "55856777050", "19626829001", "24094592008", "58063164083" };
 
-    String[] cpfsWithoutRestriction = { "172.153.098-35", "088.458.748-77", "420.591.348-62 ", "404.220.138-54",
-            "025.220.606-19", "211.246.041-20", "299.827.458-75", "288.892.178-20", "400.640.618-50" };
+    String[] cpfsWithoutRestriction = { "17215309835", "08845874877", "42059134862 ", "40422013854",
+            "02522060619", "21124604120", "29982745875", "28889217820", "40064061850" };
 
     int random = randomNumber();
 
     @Test
     public void consultCPFWithRestriction() {
-        baseURI = "http://localhost:8080/api/v1";
-
         when().get("/restricoes/{cpf}", cpfsWithRestriction[random])
                 .then().assertThat()
                 .statusCode(200).body("mensagem", equalTo("O CPF " + cpfsWithRestriction[random] + " tem problema"));
@@ -33,7 +43,6 @@ public class simulationTest {
 
     @Test
     public void consultCPFWithoutRestriction() {
-        baseURI = "http://localhost:8080/api/v1";
         when().get("/restricoes/{cpf}", cpfsWithoutRestriction[random])
                 .then().assertThat().statusCode(204);
     }
@@ -42,10 +51,10 @@ public class simulationTest {
     public void newSimulation() {
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("cpf", cpfsWithRestriction[random]);
-        map.put("nome", "Alif Corrêa Flores");
-        map.put("email", "alifcorrea.qa@gmail.com");
-        map.put("valor", 1200);
-        map.put("parcelas", 12);
+        map.put("nome", name);
+        map.put("email", name + "@gmail.com");
+        map.put("valor", monetaryValue);
+        map.put("parcelas", installments);
         map.put("seguro", true);
         JSONObject request = new JSONObject(map);
 
@@ -55,38 +64,78 @@ public class simulationTest {
     }
 
     @Test
+    public void newSimulationWithError() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("cpf", cpfsWithRestriction[random]);
+        map.put("nome", name);
+        map.put("email", name + "@gmail.com");
+        // map.put("valor", 1200);
+        map.put("parcelas", installments);
+        map.put("seguro", true);
+        JSONObject request = new JSONObject(map);
+
+        given().contentType(ContentType.JSON).body(request.toJSONString())
+                .when().post("/simulacoes")
+                .then().statusCode(400);
+    }
+
+    @Test
     public void getAllSimulations() {
-        baseURI = "http://localhost:8080/api/v1";
         when().get("/simulacoes").then().statusCode(200).log().body();
     }
 
     @Test
     public void getSimulationByCPF() {
-        baseURI = "http://localhost:8080/api/v1";
-        when().get("/simulacoes/{cpf}", "97093236014")
+        when().get("/simulacoes/{cpf}", "66414919004")
                 .then().statusCode(200);
+    }
+
+    @Test
+    public void getUnknomnSimulationByCPF() {
+        when().get("/simulacoes/{cpf}", cpfsWithoutRestriction[random])
+                .then().statusCode(404);
     }
 
     @Test
     public void editSimulation() {
         Map<String, Object> map = new HashMap<String, Object>();
-        map.put("cpf", cpfsWithRestriction[0]);
+        map.put("cpf", "66414919004");
+        map.put("nome", "Fulano de QA");
+        map.put("email", "fulano.qa@gmail.com");
+        map.put("valor", monetaryValue);
+        map.put("parcelas", installments);
+        map.put("seguro", false);
+        JSONObject request = new JSONObject(map);
+
+        given().contentType(ContentType.JSON).body(request.toJSONString())
+                .when().put("/simulacoes/{cpf}", "66414919004")
+                .then().statusCode(200);
+    }
+
+    @Test
+    public void editSimulationWithError() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("cpf", cpfsWithoutRestriction[random]);
         map.put("nome", "Alif Corrêa Flores Final");
         map.put("email", "alifcorreas.qa@gmail.com");
-        map.put("valor", 1500);
-        map.put("parcelas", 24);
+        map.put("valor", monetaryValue);
+        map.put("parcelas", installments);
         map.put("seguro", false);
         JSONObject request = new JSONObject(map);
 
         given().contentType(ContentType.JSON).body(request.toJSONString())
                 .when().put("/simulacoes/{cpf}", cpfsWithRestriction[0])
-                .then().statusCode(200);
+                .then().statusCode(404);
     }
 
     @Test
     public void deleteSimulation() {
-        when().delete("/simulacoes/{id}", 1)
+        when().delete("/simulacoes/{id}", 13)
                 .then().statusCode(200)
                 .body(equalTo("OK"));
+    }
+
+    public static int randomNumber() {
+        return (int) Math.floor(Math.random() * (9 - 0 + 1) + 0);
     }
 }
