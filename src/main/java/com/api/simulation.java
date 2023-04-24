5edacf1bf3a5019feedb263d6c07cd91bd643388
Alif Corrêa Flores <alifcorrea.qa@gmail.com>
2023-04-23 21:41:20 -0300

    create a simulation class

--- src/main/java/com/api/simulation.java
+++ src/main/java/com/api/simulation.java
@@ -1,14 +1,44 @@
 package com.api;
 
 public class simulation {
-    String nome;
-    String cpf;
-    String email;
-    int valor;
-    int parcelas;
-    Boolean seguro;
+    private String name;
+    private String cpf;
+    private String email;
+    private int valor;
+    private int parcelas;
+    private Boolean seguro;
 
-    public static void main(String[] args) {
+    public simulation(String name, String cpf, String email, int valor, int parcelas,
+            boolean seguro) {
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        this.valor = valor;
+        this.parcelas = parcelas;
+        this.seguro = seguro;
+    }
 
+    public String getname() {
+        return this.name;
+    }
+
+    public String getCPF() {
+        return this.cpf;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public int getValor() {
+        return this.valor;
+    }
+
+    public int getParcelas() {
+        return this.parcelas;
+    }
+
+    public Boolean getSeguro() {
+        return this.seguro;
     }
 }
